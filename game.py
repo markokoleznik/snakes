d38b73090b7685e63be93eb49c7b12dfdf3ff7fd
Marko Koleznik <markokoleznik@Markos-MacBook-Pro-3.local>
2014-11-28 18:17:00 +0100

    Dodana kača
    Kača z limetino in roza barvo

--- game.py
+++ game.py
@@ -12,12 +12,14 @@ import random
 
 from bolivianAnaconda import BolivianAnaconda
 from congoWaterCobra import CongoWaterCobra
+from slepec import Slepec
 
 # Nato v spisek SNAKES dodate razred, ki predstavlja vaso kaco
 
 SNAKES = [
     BolivianAnaconda,
     CongoWaterCobra,
+    Slepec,
 ]
 
 # Od tu naprej se ni treba nicesar dotikati
